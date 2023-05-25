3314f6dfe0da655c59bcb1788e47ac57e1f51a9c
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-25 11:09:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//your JS code here. If req
+
+let selref = document.getElementById("colorSelect")
+let val = selref.value;
+let btnref = document.getElementsByTagName("input")[0]
+btnref.addEventListener("click",removeval)
+function removeval(val){
+     selref.remove(val)
+}
