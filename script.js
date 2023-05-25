c502c0faa06afd9d50e44d768e4cb6cafb8729b7
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-25 11:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //your JS code here. If req
 
-let selref = document.getElementById("colorSelect")
-let val = selref.value;
+
 let btnref = document.getElementsByTagName("input")[0]
-btnref.addEventListener("click",removeval)
-function removeval(val){
-     selref.remove(val)
-}
+btnref.addEventListener("click",removeSelectedColor)
+function removeSelectedColor() {
+      var selectElement = document.getElementById("colorSelect");
+      var selectedIndex = selectElement.selectedIndex;
+      if (selectedIndex !== -1) {
+        selectElement.remove(selectedIndex);
+      }
+    }
